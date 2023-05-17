ca0c3a9351962b4d9f2b1c9450f00dfa18e0c38b
rocky-roll <federozky@gmail.com>
2023-05-17 19:58:42 -0300

    file edit

--- encrypt.py
+++ encrypt.py
@@ -6,8 +6,8 @@ def key_cript(api_key__):
     # created_key_ = Fernet.generate_key()
     # print(f"{created_key_.decode('utf-8')}")
     # created_key = Fernet(created_key_)
-    created_key = Fernet('Put-here-key-Generated-With-Fernet-Generate-Key')
-    
+    created_key = Fernet('YIzLIHGUkYdb118zIYua6A2j7cdZ1ya9nAi--W7c59Y=')
+
     cript = created_key.encrypt(str.encode(api_key__))
     # print(api_key__)
     # print(f"{created_key.decode('utf-8')}")
@@ -16,8 +16,8 @@ def key_cript(api_key__):
 
 
 def key_decript():
-    created_key = Fernet('Put-here-key-Generated-With-Fernet-Generate-Key')
-    
+    created_key = Fernet('YIzLIHGUkYdb118zIYua6A2j7cdZ1ya9nAi--W7c59Y=')
+
     f2 = open("api_key.txt", "r", encoding='utf-8')
     mk = f2.read()
     api_key_decrpt_ = created_key.decrypt(mk)
@@ -26,4 +26,3 @@ def key_decript():
 
 
 # print(key_decript().decode('utf-8'))
-
